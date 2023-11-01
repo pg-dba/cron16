0929ae6fcb6a28f9526446c76b2b292941f91ff5
Alexander <java.git@yandex.ru>
2023-11-01 09:36:15 +0300

    commit

--- c_send_report_interval.sh
+++ c_send_report_interval.sh
@@ -2,8 +2,9 @@
 # c_send_report_interval.sh
 
 FILEREPORT='/cronwork/pg_profile_interval.html'
-REPORTNAME="<b>[${HOST}]</b> Interval Report"
-MSGTEXT="<html>PostgreSQL pg_profile ${REPORTNAME}<BR><p style=\"font-family:Monospace;font-size:10px\"><a href=\"https://postgrespro.ru/docs/postgrespro/16/pgpro-pwr#PGPRO-PWR-SECTIONS-OF-A-REPORT\">Описание разделов отчёта</a><BR><a href=\"https://github.com/zubkov-andrei/pg_profile/blob/master/doc/pg_profile.md#sections-of-a-report\">Description of report sections</a></p><BR>See Attachment</html>"
+REPORTNAME="Interval Report"
+SUBJTEXT="PostgreSQL [${HOST}] pg_profile ${REPORTNAME}"
+MSGTEXT="<html>PostgreSQL <b>[${HOST}]</b> pg_profile ${REPORTNAME}<BR><p style=\"font-family:Monospace;font-size:10px\"><a href=\"https://postgrespro.ru/docs/postgrespro/16/pgpro-pwr#PGPRO-PWR-SECTIONS-OF-A-REPORT\">Описание разделов отчёта</a><BR><a href=\"https://github.com/zubkov-andrei/pg_profile/blob/master/doc/pg_profile.md#sections-of-a-report\">Description of report sections</a></p><BR>See Attachment</html>"
 SNAPS=$1
 SHIFT=$2
 
@@ -27,7 +28,7 @@ cmdsend=$(echo "mutt -e \"set content_type=text/html\" -e \"set send_charset=utf
   -e \"set from=\\\"${MAILLOGIN}\\\"\"  -e \"set realname=\\\"${MAILFROM}\\\"\" -e \"set smtp_authenticators=\\\"login\\\"\" \
   -e \"set smtp_url=smtps://\\\"${MAILLOGIN}\\\"@\\\"${MAILSMTP}\\\"\" 
   -e \"set smtp_pass=\\\"${MAILPWD}\\\"\" -e \"set ssl_starttls=yes\" -e \"set ssl_force_tls=yes\" -e \"set ssl_verify_dates=no\" -e \"set ssl_verify_host=no\" \
-  -s \"PostgreSQL ${REPORTNAME}\" -a ${FILEREPORT} -- ${MAILTO}")
+  -s \"${SUBJTEXT}\" -a ${FILEREPORT} -- ${MAILTO}")
 #echo ${cmdsend}
 
 echo "${MSGTEXT}" | \
@@ -35,7 +36,7 @@ mutt -e "set content_type=text/html" -e "set send_charset=utf-8" -e "set allow_8
   -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" -e "set smtp_authenticators=\"login\"" \
   -e "set smtp_url=smtps://\"${MAILLOGIN}\"@\"${MAILSMTP}\"" \
   -e "set smtp_pass=\"${MAILPWD}\"" -e "set ssl_starttls=yes" -e "set ssl_force_tls=yes" -e "set ssl_verify_dates=no" -e "set ssl_verify_host=no" \
-  -s "PostgreSQL pg_profile ${REPORTNAME}" -a ${FILEREPORT} -- ${MAILTO} 2>&1 | ts '[pg_profile]   '
+  -s "${SUBJTEXT}" -a ${FILEREPORT} -- ${MAILTO} 2>&1 | ts '[pg_profile]   '
 RC=$?
 echo "[pg_profile]  Send ${REPORTNAME}. RC=${RC}"
 
@@ -47,14 +48,14 @@ if [[ -v MAILSMTPURL ]]; then
 cmdsend=$(echo "mutt -e \"set ssl_starttls=no\" -e \"set ssl_force_tls=no\" -e \"set content_type=text/html\" -e \"set send_charset=utf-8\" \
   -e \"set allow_8bit=yes\" -e \"set use_ipv6=no\" \
   -e \"set from=\\\"${MAILLOGIN}\\\"\" -e \"set realname=\\\"${MAILFROM}\\\"\" -e \"set smtp_url=\\\"${MAILSMTPURL}\\\"\" \
-  -s \"PostgreSQL ${REPORTNAME}\" -a ${FILEREPORT} -- ${MAILTO}")
+  -s \"${SUBJTEXT}\" -a ${FILEREPORT} -- ${MAILTO}")
 #echo ${cmdsend}
 
 echo "${MSGTEXT}" | \
 mutt -e "set ssl_starttls=no" -e "set ssl_force_tls=no" -e "set content_type=text/html" -e "set send_charset=utf-8" \
   -e "set allow_8bit=yes" -e "set use_ipv6=no" -e "set move=no" -e "set copy=no" \
   -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" \
-  -s "PostgreSQL pg_profile ${REPORTNAME}" -a ${FILEREPORT} -- ${MAILTO} 2>&1 | ts '[pg_profile]   '
+  -s "${SUBJTEXT}" -a ${FILEREPORT} -- ${MAILTO} 2>&1 | ts '[pg_profile]   '
 RC=$?
 echo "[pg_profile]  Send ${REPORTNAME}. RC=${RC}"
 
