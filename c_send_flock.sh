202a4038d2d635fcd1fcb1e16eb38486f067efe3
Alexander <java.git@yandex.ru>
2023-11-22 10:49:57 +0300

    commit

--- c_send_flock.sh
+++ c_send_flock.sh
@@ -19,7 +19,7 @@ echo -e ${MSG} | mutt -e "set content_type=text/html" -e "set send_charset=utf-8
     -e "set realname=\"${MAILFROM}\"" -e "set smtp_authenticators=\"login\"" -e "set smtp_url=smtps://\"${MAILLOGIN}\"@\"${MAILSMTP}\"" -e "set smtp_pass=\"${MAILPWD}\"" \
     -e "set ssl_starttls=yes" -e "set ssl_force_tls=yes" -e "set ssl_verify_dates=no" -e "set ssl_verify_host=no" -s "${SUBJ}" ${MAILTO} 2>&1 | ts '[pg locks]   '
 RC=$?
-echo "[flock]  Send Flock Alert. RC=${RC}"
+echo "[flock]  Send Flock '${MSG}' Alert. RC=${RC}"
 
 fi
 
@@ -35,6 +35,6 @@ echo -e ${MSG} | mutt -e "set ssl_starttls=no" -e "set ssl_force_tls=no" -e "set
     -e "set use_ipv6=no" -e "set move=no" -e "set copy=no" -e "set from=\"${MAILLOGIN}\"" -e "set realname=\"${MAILFROM}\"" -e "set smtp_url=\"${MAILSMTPURL}\"" \
     -s "${SUBJ}" ${MAILTO} 2>&1 | ts '[pg locks]   '
 RC=$?
-echo "[flock]  Send Flock Alert. RC=${RC}"
+echo "[flock]  Send Flock '${MSG}' Alert. RC=${RC}"
 
 fi
