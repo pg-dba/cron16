a85a57d04ae43bd7feafb5f0c74a8b9ff5fa10f4
Alexander <java.git@yandex.ru>
2023-12-24 11:53:35 +0300

    commit

--- c_log_count.sh
+++ c_log_count.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # c_log_count.sh
-PGPASSWORD=${PASSWORD} PGOPTIONS="-c geqo=off -c statement_timeout=5min -c client_min_messages=error" psql -h ${HOST} -p ${PORT} -U ${USERNAME} -d ${DBNAME} -xtA -c "SELECT (SELECT count(*)::text FROM public.log_state()) as log_count ,(SELECT count(*)::text FROM public.log_state() WHERE datacheck=false) as bad_log_count;" 2>&1 | ts '[pglog] ';
+PGPASSWORD=${PASSWORD} PGOPTIONS="-c geqo=off -c statement_timeout=5min -c client_min_messages=error" psql -h ${HOST} -p ${PORT} -U ${USERNAME} -d ${DBNAME} -xtA -c "SELECT (SELECT count(*)::text FROM public.log_state()) as log_count ,(SELECT count(*)::text FROM public.log_state() WHERE datacheck=false AND dtlast >= (dtright + interval '1 sec')) as bad_log_count;" 2>&1 | ts '[pglog] ';
