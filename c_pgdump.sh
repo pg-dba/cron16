330765e412713ee2d5ca271cbc6dadd8ae845539
Alexander.R <ar-mob@ya.ru>
2024-03-15 19:09:16 +0300

    commit

--- c_pgdump.sh
+++ c_pgdump.sh
@@ -8,12 +8,26 @@ backupDatabase=$1
 #backupName="${backupDatabase}_$(date +%FT%T%z).dump"
 backupName="${backupDatabase}_$(date +%A).dump"
 
+zbxskey='pgsql.backup.start["'${backupDatabase}'"]'
+zbxfkey='pgsql.backup.finish["'${backupDatabase}'"]'
+zbxrkey='pgsql.backup.rotation["'${backupDatabase}'"]'
+zbxtkey='pgsql.backup.time["'${backupDatabase}'"]'
+
 echo "[pgdump]  [${backupDatabase}] backup started"
+bkp_start=$EPOCHSECONDS
+zabbix_sender -z ${ZBX_SERVERS} -p ${ZBX_PORT} -s ${ZBX_HOST} -k "${zbxskey}" -o "$(date --iso-8601=seconds)" 2>&1 1>/dev/null
 
 PGPASSWORD=${PASSWORD} pg_dump -h ${HOST} -p ${PORT} -U ${USERNAME} -Fc -f "/pgbackups/${backupName}" "${backupDatabase}" 2>&1
 RC=$?
 
 echo "[pgdump]  [${backupDatabase}] backup finished. RC=${RC}"
+bkp_finish=$EPOCHSECONDS
+
+if [[ $RC -eq 0 ]] ; then
+zabbix_sender -z ${ZBX_SERVERS} -p ${ZBX_PORT} -s ${ZBX_HOST} -k "${zbxfkey}" -o "$(date --iso-8601=seconds)" 2>&1 1>/dev/null
+zabbix_sender -z ${ZBX_SERVERS} -p ${ZBX_PORT} -s ${ZBX_HOST} -k "${zbxtkey}" -o "$((${bkp_finish}-${bkp_start}))" 2>&1 1>/dev/null
+fi
+
 
 if [[ ("$#" -eq 2) ]]; then
 
@@ -22,6 +36,8 @@ saves=$2
 rm -f $(ls -1t --time-style=long-iso /pgbackups/${backupDatabase}_*.dump 2>/dev/null | sed -n "$((${saves}+1)),\$p")
 
 echo "[pgdump]  [${backupDatabase}] backup file $2 rotation completed."
+
+zabbix_sender -z ${ZBX_SERVERS} -p ${ZBX_PORT} -s ${ZBX_HOST} -k "${zbxrkey}" -o "${saves}" 2>&1 1>/dev/null
 
 fi
 
